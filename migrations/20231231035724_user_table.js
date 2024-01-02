4376dbf4d8356e7a0b40a494bf1e683ecdab677a
clinton bess <clintonbess3@gmail.com>
2024-01-02 11:37:21 -0800

    adds test user to user table when db is created

--- migrations/20231231035724_user_table.js
+++ migrations/20231231035724_user_table.js
@@ -1,11 +1,17 @@
-const { TableBuilder } = require('knex')
+exports.up = async function (knex) {
+  await knex.transaction(async (trx) => {
+    await trx.schema.createTable('users', function (table) {
+      table.increments('id').primary()
+      table.string('username').notNullable().unique()
+      table.string('password').notNullable()
+      table.boolean('deleted').defaultTo(false)
+    })
 
-exports.up = function (knex) {
-  return knex.schema.createTable('users', function (table) {
-    table.increments('id').primary()
-    table.string('username').notNullable().unique()
-    table.string('password').notNullable()
-    table.boolean('deleted').defaultTo(false)
+    await trx('users').insert({
+      username: 'test',
+      password: '$2a$10$32pclAuxAdX555CG/rQtXuNIQCQnRqjmloXS0X6F8b6eJ2pDv2utO',
+      deleted: false,
+    })
   })
 }
 
